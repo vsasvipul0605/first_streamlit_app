ae1fbfef604fb91dce88e8aeb4c2a27f2e2d19c7
Vipul Sinha <vsasvipul@gmail.com>
2023-03-15 21:59:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
+import pandas
 
-streamlit.title("Snoeflake Trial")
+streamlit.title("Snowflake Trial")
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
